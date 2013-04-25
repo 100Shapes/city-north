68689e6689db8eeb381eef2068d849e53e54b108
Paolo Memoli <paolo@100shapes.com>
2013-04-25 16:54:47 +0100

    added all html content, started styling

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+$(function() {  
+  $(".submit").click(function(e) {
+  	console.log("trying...");
+    $.ajax({  
+  		type: "POST",  
+  		url: "http://100shapes.wufoo.com/forms/z7x3p3/#public",
+  		data: $("#form1").serialize(),  
+  		success: function(data, textStatus, jqXHR) {  
+    		console.log(data, textStatus, jqXHR);
+		},
+		error: function(jqXHR, textStatus, errorMessage) {
+           console.log(jqXHR.getAllResponseHeaders(), textStatus, errorMessage); // Optional
+        }
+	});
+	window.location.href="thanks.html";
+	return false;
+  });  
+});
