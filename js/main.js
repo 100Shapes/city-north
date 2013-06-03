cb2a6f442d9dc8796fcca97ef887a6805029c030
Paolo Memoli <paolo@100shapes.com>
2013-06-03 16:17:47 +0100

    added second form, redid type

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 $(function() {  
-  $(".submit").click(function(e) {
+  $(".submit-residential").click(function(e) {
   	console.log("trying...");
     $.ajax({  
   		type: "POST",  
@@ -18,5 +18,25 @@ $(function() {
 	});
 
 	return false;
+  });  
+
+  $(".submit-commercial").click(function(e) {
+    console.log("trying...");
+    $.ajax({  
+      type: "POST",  
+      url: "http://100shapes.wufoo.com/forms/m7x3z9/#public",
+      data: $("#form1").serialize(),  
+      success: function(data, textStatus, jqXHR) {  
+        console.log(data, textStatus, jqXHR);
+
+        window.location.href="thanks.html";
+    },
+    error: function(jqXHR, textStatus, errorMessage) {
+           console.log(jqXHR.getAllResponseHeaders(), textStatus, errorMessage); // Optional
+           window.location.href="thanks.html";
+        }
+  });
+
+  return false;
   });  
 });
