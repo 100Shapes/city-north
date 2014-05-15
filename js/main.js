e79c55cc13047c7aa14a677edd01730bbbf73a3b
Paolo Memoli <paolo@100shapes.com>
2014-05-15 17:55:40 +0100

    added emailer page

--- js/main.js
+++ js/main.js
@@ -50,5 +50,34 @@ $(function() {
 
   return false;
   });  
+
+  $(".form-emailer").submit(function(e) {
+    console.log("trying...");
+    if (($('#Field14').val().length>0)&&($('#Field15').val().length>0)&&($('#Field16').val().length>0)){
+      console.log ('good');
+      $.ajax({  
+        type: "POST",  
+        url: "http://100shapes.wufoo.com/forms/q11hfxky107rt5l/#public",
+        data: $("#form1").serialize(),  
+        success: function(data, textStatus, jqXHR) {  
+          console.log(data, textStatus, jqXHR);
+
+          window.location.href="thanks.html";
+        },
+        error: function(jqXHR, textStatus, errorMessage) {
+               console.log(jqXHR.getAllResponseHeaders(), textStatus, errorMessage); // Optional
+               window.location.href="thanks.html";
+        }
+      });
+    } else {
+      console.log ('bad');
+      $('.hidden').removeClass('hidden');
+    }
+
+  return false;
+  });  
+
+
+
 });
 
